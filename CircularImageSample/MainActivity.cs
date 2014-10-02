ea5e2408af844399f6c09fdba2845112be125acd
Marc Obaldo <marcklaser@gmail.com>
2014-10-02 15:41:36 +0800

    Added project files

---
+++ CircularImageSample/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Codewisp.Xamarin.Android.CircularImageSample
+{
+    [Activity(Label = "Codewisp.Xamarin.Android.CircularImageSample", MainLauncher = true, Theme = "@android:style/Theme.Light.NoTitleBar", Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
